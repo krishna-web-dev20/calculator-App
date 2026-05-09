9cd80439b542e2a436dd50c1cac0210ef9f32635
Desai Krishna Karthik <kkarthikdesai2820@gmail.com>
2026-05-09 23:41:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const display =
+document.getElementById("display");
+
+const buttons =
+document.querySelectorAll("button");
+
+let expression = ""
+
+buttons.forEach((button) => {
+    
+    button.addEventListener("click", () => {
+
+        const value = button.innerText;
+
+        if(value === "C"){
+            expression = "";
+            display.value = "";
+        }
+
+        else if(value === "="){
+
+            try{
+                expression = eval(expression);
+                display.value = expression;
+            }
+
+            catch{
+                display.value = "Error";
+            }
+        }
+            else{
+                expression += value;
+                display.value = expression;
+            }
+        
+    })
+})
